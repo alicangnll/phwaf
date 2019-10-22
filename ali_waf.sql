f7c961f88e4a2ab1636e5f432b9807b139f962ca
Ali Can <alicangonullu@outlook.com>
2019-10-23 00:05:34 +0300

    pH Analyzer Patch 4
    pH Analyzer Patch 4

--- ali_waf.sql
+++ ali_waf.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Anamakine: 127.0.0.1
--- Üretim Zamanı: 13 Eki 2019, 13:30:23
+-- Üretim Zamanı: 22 Eki 2019, 23:05:11
 -- Sunucu sürümü: 10.4.6-MariaDB
 -- PHP Sürümü: 7.1.31
 
@@ -62,7 +62,7 @@ CREATE TABLE `guard_watch` (
 --
 
 INSERT INTO `guard_watch` (`kural_id`, `kural_adi`, `kural_hakkinda`, `kural_icerik`) VALUES
-(1, 'SQL Engelleme Kuralı', 'SQL Zaafiyeti Engelleme', '..¿¿½¿¿%¿¿OR¿¿\"¿¿\'¿¿`¿¿concat¿¿join¿¿\\\"¿¿\\\\¿¿tablo_adim¿¿kolon_adim¿¿script¿¿alert¿¿JaVaScRiPT¿¿\"¿¿fromCharCode¿¿onmouseover¿¿&#¿¿#&¿¿	¿¿!¿¿#¿¿$¿¿%¿¿(¿¿)¿¿*¿¿~¿¿+¿¿-¿¿_¿¿,¿¿:¿¿;¿¿@[¿¿/¿¿|¿¿\\¿¿]¿¿^¿¿`¿¿//¿¿/¿¿livescript¿¿constructor¿¿:¿¿\'\\ja\\vasc\\ript:alert¿¿¾¿¿¢¿¿order¿¿by¿¿union¿¿table_name¿¿column_name¿¿from¿¿concat¿¿and¿¿AND¿¿limit¿¿SRC¿¿src¿¿meta¿¿embed¿¿xml¿¿XML¿¿x;¿¿x27;¿¿quot¿¿+¿¿UNION¿¿-¿¿-¿¿+¿¿*¿¿’¿¿|¿¿\"¿¿?¿¿₺¿¿@¿¿,¿¿/¿¿“¿¿€¿¿£¿¿$¿¿~¿¿\\¿¿[¿¿]¿¿{¿¿}¿¿'),
+(4, 'SQL Engelleme', '', '..¿¿½¿¿%¿¿OR¿¿\"¿¿\'¿¿`¿¿concat¿¿join¿¿\\\"¿¿\\\\¿¿tablo_adim¿¿kolon_adim¿¿script¿¿alert¿¿JaVaScRiPT¿¿\"¿¿fromCharCode¿¿onmouseover¿¿&#¿¿#&¿¿	¿¿!¿¿#¿¿$¿¿%¿¿(¿¿)¿¿*¿¿~¿¿+¿¿-¿¿_¿¿,¿¿:¿¿;¿¿@[¿¿/¿¿|¿¿\\¿¿]¿¿^¿¿`¿¿//¿¿/¿¿livescript¿¿constructor¿¿:¿¿\'\\ja\\vasc\\ript:alert¿¿¾¿¿¢¿¿order¿¿by¿¿union¿¿table_name¿¿column_name¿¿from¿¿concat¿¿and¿¿AND¿¿limit¿¿SRC¿¿src¿¿meta¿¿embed¿¿xml¿¿XML¿¿x;¿¿x27;¿¿quot¿¿+¿¿UNION¿¿-¿¿-¿¿+¿¿*¿¿’¿¿|¿¿\"¿¿?¿¿₺¿¿@¿¿,¿¿/¿¿“¿¿€¿¿£¿¿$¿¿~¿¿\\¿¿[¿¿]¿¿{¿¿}¿¿>¿¿<¿¿'),
 (2, 'XSS Engelleme Kuralı', 'XSS Engelleyici', '..¿¿½¿¿%¿¿OR¿¿\\\"¿¿\\\'¿¿`¿¿concat¿¿join¿¿\\\\\\\"¿¿\\\\\\\\¿¿tablo_adim¿¿kolon_adim¿¿script¿¿alert¿¿JaVaScRiPT¿¿\"¿¿fromCharCode¿¿onmouseover¿¿&#¿¿#&¿¿	¿¿!¿¿#¿¿$¿¿%¿¿(¿¿)¿¿*¿¿~¿¿+¿¿-¿¿_¿¿,¿¿:¿¿;¿¿@[¿¿/¿¿|¿¿\\\\¿¿]¿¿^¿¿`¿¿//¿¿/¿¿livescript¿¿constructor¿¿:¿¿\\\'\\\\ja\\\\vasc\\\\ript:alert¿¿¾¿¿¢¿¿order¿¿by¿¿union¿¿table_name¿¿column_name¿¿from¿¿concat¿¿and¿¿AND¿¿limit¿¿SRC¿¿src¿¿meta¿¿embed¿¿xml¿¿XML¿¿x;¿¿lt¿¿>¿¿x27;¿¿quot¿¿+¿¿UNION¿¿-¿¿-¿¿+¿¿*¿¿’¿¿?¿¿'),
 (3, 'RFI Engelleme', 'RFI Engelleme', '//¿¿etc¿¿passwd¿¿conf¿¿MYD¿¿MYI¿¿ini¿¿{¿¿}¿¿/¿¿#¿¿[¿¿]¿¿&#37;¿¿@¿¿¢¿¿¤¿¿¥¿¿¦¿¿§¿¿©¿¿ª¿¿«¿¿¬¿¿&shy;¿¿®¿¿¯¿¿°¿¿±¿¿²¿¿³¿¿µ¿¿¶¿¿·¿¿¸¿¿¹¿¿º¿¿¼¿¿¾¿¿À¿¿exec¿¿%¿¿unhex¿¿ CONVERT¿¿CONCAT_WS¿¿CHAR¿¿InfORmaTion_scHema¿¿WHERE¿¿hex¿¿%20¿¿AND¿¿substring¿¿version¿¿ascii¿¿SLEEP¿¿md5¿¿%0C¿¿%A0¿¿MID¿¿LIKE¿¿');
 
@@ -89,12 +89,32 @@ INSERT INTO `guncelleme_table` (`guncelleme_id`, `guncelleme_adi`, `guncelleme_k
 -- --------------------------------------------------------
 
 --
+-- Tablo için tablo yapısı `header_blok`
+--
+
+CREATE TABLE `header_blok` (
+  `ayar_id` int(11) NOT NULL,
+  `ayar_adi` varchar(255) COLLATE utf8_turkish_ci NOT NULL,
+  `ayar_icerik` text COLLATE utf8_turkish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
+
+--
+-- Tablo döküm verisi `header_blok`
+--
+
+INSERT INTO `header_blok` (`ayar_id`, `ayar_adi`, `ayar_icerik`) VALUES
+(1, 'SQL POST Enjeksiyon', '¿¿[¿¿]¿¿');
+
+-- --------------------------------------------------------
+
+--
 -- Tablo için tablo yapısı `ip_ban`
 --
 
 CREATE TABLE `ip_ban` (
   `ip_id` int(11) NOT NULL,
   `ip_adresi` varchar(255) NOT NULL,
+  `ip_usragent` text NOT NULL,
   `ip_suresi` time NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
@@ -102,8 +122,8 @@ CREATE TABLE `ip_ban` (
 -- Tablo döküm verisi `ip_ban`
 --
 
-INSERT INTO `ip_ban` (`ip_id`, `ip_adresi`, `ip_suresi`) VALUES
-(1, '127.0.0.1', '00:00:00');
+INSERT INTO `ip_ban` (`ip_id`, `ip_adresi`, `ip_usragent`, `ip_suresi`) VALUES
+(6, '::1', '{\"Host\":\"localhost\",\"Connection\":\"keep-alive\",\"Cache-Control\":\"max-age=0\",\"Upgrade-Insecure-Requests\":\"1\",\"User-Agent\":\"Mozilla\\/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit\\/537.36 (KHTML, like Gecko) Chrome\\/77.0.3865.120 Safari\\/537.36\",\"Sec-Fetch-Mode\":\"navigate\",\"Sec-Fetch-User\":\"?1\",\"Accept\":\"text\\/html,application\\/xhtml+xml,application\\/xml;q=0.9,image\\/webp,image\\/apng,*\\/*;q=0.8,application\\/signed-exchange;v=b3\",\"Sec-Fetch-Site\":\"none\",\"Accept-Encoding\":\"gzip, deflate, br\",\"Accept-Language\":\"tr-TR,tr;q=0.9,en-US;q=0.8,en;q=0.7\",\"Cookie\":\"PHPSESSID=h50snqj6cdd53hupmkqvefakge\"}', '23:04:25');
 
 -- --------------------------------------------------------
 
@@ -124,7 +144,7 @@ CREATE TABLE `method_blok` (
 
 INSERT INTO `method_blok` (`method_id`, `method_adi`, `method_bilgisi`, `method_turu`) VALUES
 (2, 'POST', 'POST', 'POST'),
-(4, 'GET', 'GET', 'GET');
+(3, 'GET', '', 'GET');
 
 -- --------------------------------------------------------
 
@@ -145,7 +165,7 @@ CREATE TABLE `waf_ayar` (
 --
 
 INSERT INTO `waf_ayar` (`ayar_id`, `ayar_adi`, `waf_aktif`, `ayar_aktif`, `oto_ban`) VALUES
-(1, 'Yeni', '1', '1', '0');
+(1, 'Yeni', '1', '1', '1');
 
 --
 -- Dökümü yapılmış tablolar için indeksler
@@ -168,6 +188,12 @@ ALTER TABLE `guard_watch`
 --
 ALTER TABLE `guncelleme_table`
   ADD PRIMARY KEY (`guncelleme_id`);
+
+--
+-- Tablo için indeksler `header_blok`
+--
+ALTER TABLE `header_blok`
+  ADD PRIMARY KEY (`ayar_id`);
 
 --
 -- Tablo için indeksler `ip_ban`
@@ -195,13 +221,31 @@ ALTER TABLE `waf_ayar`
 -- Tablo için AUTO_INCREMENT değeri `guard_watch`
 --
 ALTER TABLE `guard_watch`
-  MODIFY `kural_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `kural_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Tablo için AUTO_INCREMENT değeri `guncelleme_table`
 --
 ALTER TABLE `guncelleme_table`
   MODIFY `guncelleme_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Tablo için AUTO_INCREMENT değeri `header_blok`
+--
+ALTER TABLE `header_blok`
+  MODIFY `ayar_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Tablo için AUTO_INCREMENT değeri `ip_ban`
+--
+ALTER TABLE `ip_ban`
+  MODIFY `ip_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- Tablo için AUTO_INCREMENT değeri `method_blok`
+--
+ALTER TABLE `method_blok`
+  MODIFY `method_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Tablo için AUTO_INCREMENT değeri `waf_ayar`
