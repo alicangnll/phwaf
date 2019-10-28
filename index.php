ff102bdb53a251277f34c941e7e143dc56bb7e93
Ali Can Gönüllü <alicangonullu1907@gmail.com>
2019-10-28 15:02:27 +0300

    AliWAF Patch 5
    AliWAF Patch 5

--- index.php
+++ index.php
@@ -86,60 +86,7 @@ a{
 	<title>pH Analyzer | AliWAF</title>  
 </head>
 <?php
-function reel_ip()  
-{  
-    if (!empty($_SERVER['HTTP_CLIENT_IP']))  
-    {  
-        $ip=$_SERVER['HTTP_CLIENT_IP'];  
-    }  
-    elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) //Proxy den bağlanıyorsa gerçek IP yi alır.
-     
-    {  
-        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];  
-    }
-   elseif (!empty($_SERVER['HTTP_X_FORWARDED'])) //Proxy den bağlanıyorsa gerçek IP yi alır.
-     
-    {  
-        $ip = $_SERVER['HTTP_X_FORWARDED'];  
-    }
-	
-   elseif (!empty($_SERVER['HTTP_X_CLUSTER_CLIENT_IP'])) //Proxy den bağlanıyorsa gerçek IP yi alır.
-     
-    {  
-        $ip = $_SERVER['HTTP_X_CLUSTER_CLIENT_IP'];  
-    } 
-	   elseif (!empty($_SERVER['HTTP_FORWARDED'])) //Proxy den bağlanıyorsa gerçek IP yi alır.
-     
-    {  
-        $ip = $_SERVER['HTTP_FORWARDED'];  
-    } 
-    else  
-    {  
-        $ip=$_SERVER['REMOTE_ADDR'];  
-    }  
-    return $ip;  
-}
-error_reporting(0);
-try {
-	$ip = "localhost"; //host
-	$user = "root";  // host id
-	$password = "";  // password local olduğu için varsayılan şifre
-	$dbad = "ali_waf"; // db adı 
-	
-     $db = new PDO("mysql:host=$ip;dbname=$dbad", "$user", "$password");
-     $db->query("SET CHARACTER SET 'utf8'");
-     $db->query("SET NAMES 'utf8'");
-
-} catch ( PDOException $e ){
-     echo '
-	 <table>
-<center><img src="veri/sql.png" alt="Örnek Resim"/></center>
-<center>No MySQL Connection</center>
-<center>Bunun Sebebi Bir DDoS Saldırısı Olabilir</center>
-<center>Sistem Yöneticinizle Irtibata Geçin</center>
-	 </table>';
-	 die();
-}
+include("libs/libs.php");
 if(!isset($_GET['git'])) {
 $sayfa = 'giris';	// eğer boşsa anasayfa varsayalım.
 } else {
