31b88cefd04b2da95af3204db00af15a8247f1bf
Ali Can Gönüllü <alicangonullu1907@gmail.com>
2019-11-13 19:39:50 +0300

    AliWAF Patch 6.1
    AliWAF Patch 6.1

--- libs/libs.php
+++ libs/libs.php
@@ -3,301 +3,132 @@ include("conn.php");
 header("X-Frame-Options: SAMEORIGIN");
 function style() {
 ?>
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
- <head>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
-    <link rel="stylesheet" media="screen" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600" />
-    <link rel="stylesheet" media="screen" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" />
+<style>
+@import 'https://fonts.googleapis.com/css?family=Inconsolata';
 
+html {
+  min-height: 100%;
+}
 
-    <style>
-     * {
-         -moz-box-sizing:border-box;
-         -webkit-box-sizing:border-box;
-         box-sizing:border-box;
-     }
+body {
+  box-sizing: border-box;
+  height: 100%;
+  background-color: #000000;
+  background-image: radial-gradient(#11581E, #041607), url("https://media.giphy.com/media/oEI9uBYSzLpBK/giphy.gif");
+  background-repeat: no-repeat;
+  background-size: cover;
+  font-family: 'Inconsolata', Helvetica, sans-serif;
+  font-size: 1.5rem;
+  color: rgba(128, 255, 128, 0.8);
+  text-shadow:
+      0 0 1ex rgba(51, 255, 51, 1),
+      0 0 2px rgba(255, 255, 255, 0.8);
+}
 
-     html, body, div, span, object, iframe, h1, h2, h3, h4, h5, h6, p, blockquote, pre,
-     abbr, address, cite, code, del, dfn, em, img, ins, kbd, q, samp,
-     small, strong, sub, sup, var, b, i, dl, dt, dd, ol, ul, li,
-     fieldset, form, label, legend, caption, article, aside, canvas, details, figcaption, figure,  footer, header, hgroup,
-     menu, nav, section, summary, time, mark, audio, video {
-         margin:0;
-         padding:0;
-         border:0;
-         outline:0;
-         vertical-align:baseline;
-         background:transparent;
-     }
+.noise {
+  pointer-events: none;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-image: url("https://media.giphy.com/media/oEI9uBYSzLpBK/giphy.gif");
+  background-repeat: no-repeat;
+  background-size: cover;
+  z-index: -1;
+  opacity: .02;
+}
 
-     article, aside, details, figcaption, figure, footer, header, hgroup, nav, section {
-         display: block;
-     }
+.overlay {
+  pointer-events: none;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background:
+      repeating-linear-gradient(
+      180deg,
+      rgba(0, 0, 0, 0) 0,
+      rgba(0, 0, 0, 0.3) 50%,
+      rgba(0, 0, 0, 0) 100%);
+  background-size: auto 4px;
+  z-index: 1;
+}
 
-     html {
-         font-size: 16px;
-         line-height: 24px;
-         width:100%;
-         height:100%;
-         -webkit-text-size-adjust: 100%;
-         -ms-text-size-adjust: 100%;
-         overflow-y:scroll;
-         overflow-x:hidden;
-     }
+.overlay::before {
+  content: "";
+  pointer-events: none;
+  position: absolute;
+  display: block;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  width: 100%;
+  height: 100%;
+  background-image: linear-gradient(
+      0deg,
+      transparent 0%,
+      rgba(32, 128, 32, 0.2) 2%,
+      rgba(32, 128, 32, 0.8) 3%,
+      rgba(32, 128, 32, 0.2) 3%,
+      transparent 100%);
+  background-repeat: no-repeat;
+  animation: scan 7.5s linear 0s infinite;
+}
 
-     img {
-         vertical-align:middle;
-         max-width: 100%;
-         height: auto;
-         border: 0;
-         -ms-interpolation-mode: bicubic;
-     }
+@keyframes scan {
+  0%        { background-position: 0 -100vh; }
+  35%, 100% { background-position: 0 100vh; }
+}
 
-     body {
-         min-height:100%;
-         -webkit-font-smoothing: subpixel-antialiased;
-     }
+.terminal {
+  box-sizing: inherit;
+  position: absolute;
+  height: 100%;
+  width: 1000px;
+  max-width: 100%;
+  padding: 4rem;
+  text-transform: uppercase;
+}
 
-     .clearfix {
-	       clear:both;
-	       zoom: 1;
-     }
-     .clearfix:before, .clearfix:after {
-         content: "\0020";
-         display: block;
-         height: 0;
-         visibility: hidden;
-     }
-     .clearfix:after {
-         clear: both;
-     }
-  body.background.error-page-wrapper, .background.error-page-wrapper.preview {
-    font-family: 'Source Sans Pro', sans-serif;
-    background-position:center center;
-    background-repeat:no-repeat;
-    background-size:cover;
-    position:relative;
-  }
+.output {
+  color: rgba(128, 255, 128, 0.8);
+  text-shadow:
+      0 0 1px rgba(51, 255, 51, 0.4),
+      0 0 2px rgba(255, 255, 255, 0.8);
+}
 
-  .background.error-page-wrapper .content-container {
-    border-radius: 6px;
-    text-align: center;
-    box-shadow: 0 0 20px rgba(0,0,0,.2);
-    padding:50px;
-    background-color: #fff;
-    width:100%;
-    max-width:525px;
-    position:absolute;
-    left:50%;
-    margin-left:-262px;
-  }
+.output::before {
+  content: "> ";
+}
 
-  .background.error-page-wrapper .content-container.in {
-    left:0px;
-    opacity:1;
-  }
+/*
+.input {
+  color: rgba(192, 255, 192, 0.8);
+  text-shadow:
+      0 0 1px rgba(51, 255, 51, 0.4),
+      0 0 2px rgba(255, 255, 255, 0.8);
+}
 
-  .background.error-page-wrapper .head-line {
-    transition:color .2s linear;
-    font-size:48px;
-    line-height:60px;
-    letter-spacing: -1px;
-    margin-bottom: 5px;
-    color:#ccc;
-  }
-  .background.error-page-wrapper .subheader {
-    transition:color .2s linear;
-    font-size:36px;
-    line-height:46px;
-    color:#333;
-  }
-  .background.error-page-wrapper .hr {
-    height:1px;
-    background-color: #ddd;
-    width:60%;
-    max-width:250px;
-    margin:35px auto;
-  }
-  .background.error-page-wrapper .context {
-    transition:color .2s linear;
-    font-size:18px;
-    line-height:27px;
-    color:#bbb;
-  }
-  .background.error-page-wrapper .context p {
-    margin:0;
-  }
-  .background.error-page-wrapper .context p:nth-child(n+2) {
-    margin-top:16px;
-  }
-  .background.error-page-wrapper .buttons-container {
-    margin-top: 35px;
-    overflow: hidden;
-  }
-  .background.error-page-wrapper .buttons-container a {
-    transition:text-indent .2s ease-out, color .2s linear, background-color .2s linear;
-    text-indent: 0px;
-    font-size:14px;
-    text-transform: uppercase;
-    text-decoration: none;
-    color:#fff;
-    background-color:#2ecc71;
-    border-radius: 99px;
-    padding:12px 0 13px;
-    text-align: center;
-    display:inline-block;
-    overflow: hidden;
-    position: relative;
-    width:45%;
-  }
+.input::before {
+  content: "$ ";
+}
+*/
 
-  .background.error-page-wrapper .buttons-container .fa {
-    transition:left .2s ease-out;
-    position: absolute;
-    left:-50px;
-  }
+a {
+  color: #fff;
+  text-decoration: none;
+}
 
-  .background.error-page-wrapper .buttons-container a:hover {
-    text-indent: 15px;
-  }
+a::before {
+  content: "[";
+}
 
-  .background.error-page-wrapper .buttons-container a:nth-child(1) {
-    float:left;
-  }
+a::after {
+  content: "]";
+}
 
-  .background.error-page-wrapper .buttons-container a:nth-child(2) {
-    float:right;
-  }
-
-  .background.error-page-wrapper .buttons-container .fa-home {
-    font-size:18px;
-    top:15px;
-  }
-
-  .background.error-page-wrapper .buttons-container a:hover .fa-home {
-    left:10px;
-  }
-
-  .background.error-page-wrapper .buttons-container .fa-warning {
-    font-size:16px;
-    top:17px;
-  }
-
-  .background.error-page-wrapper .buttons-container a:hover .fa-warning {
-    left:5px;
-  }
-
-  .background.error-page-wrapper .buttons-container .fa-power-off {
-    font-size:16px;
-    top:17px;
-  }
-
-  .background.error-page-wrapper .buttons-container a:hover .fa-power-off {
-    left:9px;
-  }
-
-
-  .background.error-page-wrapper .buttons-container.single-button {
-    text-align: center;
-  }
-
-  .background.error-page-wrapper .buttons-container.single-button a  {
-    float:none !important;
-  }
-
-  @media screen and (max-width: 555px) {
-    .background.error-page-wrapper {
-      padding:30px 5%;
-    }
-    .background.error-page-wrapper .content-container {
-      padding:37px;
-      position: static;
-      left: 0;
-      margin-left:0;
-    }
-    .background.error-page-wrapper .head-line {
-      font-size:36px;
-    }
-    .background.error-page-wrapper .subheader {
-      font-size:27px;
-      line-height: 37px;
-    }
-    .background.error-page-wrapper .hr {
-      margin:30px auto;
-      width:215px;
-    }
-    .background.error-page-wrapper .buttons-container .fa {
-      display:none;
-    }
-    .background.error-page-wrapper .buttons-container a:hover {
-      text-indent: 0px;
-    }
-  }
-
-  @media screen and (max-width: 450px) {
-    .background.error-page-wrapper {
-      padding:30px;
-    }
-    .background.error-page-wrapper .head-line {
-      font-size:32px;
-    }
-    .background.error-page-wrapper .hr {
-      margin:25px auto;
-      width:180px;
-    }
-    .background.error-page-wrapper .context {
-      font-size:15px;
-      line-height:22px;
-    }
-
-    .background.error-page-wrapper .context p:nth-child(n+2) {
-      margin-top:10px;
-    }
-    .background.error-page-wrapper .buttons-container {
-      margin-top:29px;
-    }
-    .background.error-page-wrapper .buttons-container a {
-      float:none !important;
-      width:65%;
-      margin:0 auto;
-      font-size:13px;
-      padding:9px 0;
-    }
-
-    .background.error-page-wrapper .buttons-container a:nth-child(2) {
-      margin-top:12px;
-    }
-  }
-    .background-image {
-      background-color: #FFFFFF;
-      background-image: url(https://archive.is/QjJ75/e499aa8e2369ac30ebfee0d75f41c75665d61b03) !important;
-    }
-
-    .primary-text-color {
-      color: rgba(107, 94, 94, 1) !important;
-    }
-
-    .secondary-text-color {
-      color: #AAAAAA !important;
-    }
-
-
-
-
-    .border-button {
-      color: rgba(255, 0, 0, 1) !important;
-      border-color: rgba(255, 0, 0, 1) !important;
-    }
-    .button {
-      background-color: rgba(255, 0, 0, 1) !important;
-      color: rgba(255, 255, 255, 1) !important;
-    }
-
-    .shadow {
-      box-shadow: 0 0 30px #000000;
-    }
-
+.errorcode {
+  color: white;
+}
 </style>
 <?php
 }
@@ -349,59 +180,35 @@ $jsonk = var_dump(json_encode($yasakla));
 
 function Error($ip, $url, $usragent, $tarih, $tur) {
 if(substr($_SERVER['HTTP_ACCEPT_LANGUAGE'],0,2) == "tr") {
-	echo '<body class="background error-page-wrapper background-color background-image">
-    <center>
-  <div class="content-container shadow">
-  <br>
-    <div class="head-line secondary-text-color">
-		Illegal Girişim Algılandı | '.$tur.'
-    </div>
-	<div class="hr"></div>
-    <div class="context primary-text-color">
-      Deneme Türü : '.$tur.' Girişimi ('.$ip.')
-    </div>
-    <div class="hr"></div>
-    <div class="context secondary-text-color">
-	<p>IP Adresi : '.$ip.'</p>
-      <p>URL : '.$url.'<br></p>
-	  <p>User-Agent : '.$usragent.'<br></p>
-	  <p>Tarih : '.$tarih.'</p>
-    </div>
+echo '<div class="noise"></div>
+<div class="overlay"></div>
+<div class="terminal">
+  <h1>Illegal Kod Algılandı <span class="errorcode">('.$tur.')</span></h1>
+  <p class="output">IP Adresiniz : <span class="errorcode">'.$ip.'</span></p>
+  <p class="output">User Agent : <span class="errorcode">'.$usragent.'</span></p>
+  <p class="output">Tarih : <span class="errorcode">'.$tarih.'</span></p>
     <div class="buttons-container">
-      <a class="button" onclick="history.back();" target="_blank"><span class="fa fa-home"></span> Geri Dön</a>
-      <a class="button" href="mailto:alicangonullu@yahoo.com" target="_blank"><span class="fa fa-warning"></span> Problem Bildir</a>
+      <a class="button" onclick="history.back();" target="_blank"><span class="fa fa-home"></span>Geri Dön</a>
+      <a class="button" href="mailto:alicangonullu@yahoo.com" target="_blank"><span class="fa fa-warning"></span>Problem Bildir</a>
       <a class="button" href="https://github.com/alicangonullu/" target="_blank"><span class="fa fa-github"></span>GitHub</a>
     </div>
-  </div>
-</center>   
-</body>';
+</div>';	
+ die();
 } else {
-	echo '<body class="background error-page-wrapper background-color background-image">
-    <center>
-  <div class="content-container shadow">
-  <br>
-    <div class="head-line secondary-text-color">
-		Illegal Try Detected | '.$tur.'
-    </div>
-	<div class="hr"></div>
-    <div class="context primary-text-color">
-      Try Type : '.$tur.' Girişimi ('.$ip.')
-    </div>
-    <div class="hr"></div>
-    <div class="context secondary-text-color">
-	<p>IP Adresi : '.$ip.'</p>
-      <p>URL : '.$url.'<br></p>
-	  <p>User-Agent : '.$usragent.'<br></p>
-	  <p>Date : '.$tarih.'</p>
-    </div>
+ echo '<div class="noise"></div>
+<div class="overlay"></div>
+<div class="terminal">
+  <h1>Illegal Code Detected <span class="errorcode">('.$tur.')</span></h1>
+  <p class="output">IP Adress : <span class="errorcode">'.$ip.'</span></p>
+  <p class="output">User Agent : <span class="errorcode">'.$usragent.'</span></p>
+  <p class="output">Date  : <span class="errorcode">'.$tarih.'</span></p>
     <div class="buttons-container">
-      <a class="button" onclick="history.back();" target="_blank"><span class="fa fa-home"></span> Back</a>
-      <a class="button" href="mailto:alicangonullu@yahoo.com" target="_blank"><span class="fa fa-warning"></span> Notify Problem</a>
-      <a class="button" href="https://github.com/alicangonullu/" target="_blank"><span class="fa fa-github"></span>GitHub Link</a>
+      <a class="button" onclick="history.back();" target="_blank"><span class="fa fa-home"></span>Back</a>
+      <a class="button" href="mailto:alicangonullu@yahoo.com" target="_blank"><span class="fa fa-warning"></span>Notify Problem</a>
+      <a class="button" href="https://github.com/alicangonullu/" target="_blank"><span class="fa fa-github"></span>GitHub</a>
     </div>
-  </div>
-</center>   
-</body>';
+</div>';
+ die();
 }
 
 }
