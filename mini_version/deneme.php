2bc6118ac84292fa96654ef1de8ea584474d923b
Ali Can Gönüllü <alicangonullu1907@gmail.com>
2020-10-27 20:48:34 +0300

    PHP WAF Mini Version (PHP 7.X Support)
    PHP WAF Mini Version (PHP 7.X Support)

--- mini_version/deneme.php
+++ mini_version/deneme.php
@@ -7,7 +7,22 @@ $sayfa = 'giris';	// eğer boşsa anasayfa varsayalım.
 }
 switch($sayfa) {
 case 'giris':
+echo '<center><a href="deneme.php?git=post">POST Filter</a></center>
+<hr></hr>';
 phpinfo();
+break;
+
+case 'post':
+echo '<center><form action="deneme.php?git=ppost" method="post">
+<label for="fname">İsim:</label>
+<input type="text" id="d" name="d"><br><br>
+<input type="submit" value="Submit">
+</form></center>';
+break;
+
+case 'ppost':
+$post = strip_tags($_POST["d"]);
+echo $post;
 break;
 }
 ?>
