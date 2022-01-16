f9fc6252998464d0574370262792924c7b290c02
Ali Can Gönüllü <alicangonullu@yahoo.com>
2022-01-16 13:53:58 +0300

    PHP WAF New Version
    PHP WAF New Version

---
+++ class.engelle.php
@@ -0,0 +1,269 @@
+<?php
+class AliWAF_Block {
+	protected $host = "localhost";
+	protected $dbname = "ali_waf";
+	protected $user = "root";
+	protected $pass = "";
+	protected $aliwaf;
+
+	public function Baglanti() {
+		try {
+			$this->aliwaf = new PDO("mysql:host=$this->host;dbname=$this->dbname", $this->user, $this->pass);
+			$this->aliwaf->query("SET CHARACTER SET 'utf8'");
+			$this->aliwaf->query("SET NAMES 'utf8'");
+		} catch (PDOException $e) {
+			die("<b>SQL Sunucusuna Bağlanılamadı : ".$e->getMessage()."</b>");
+		}
+	}
+
+	public function closeConnection() {
+		$this->alwaf = null;
+	}
+
+	public function reel_ip() {
+    if (!empty($_SERVER['HTTP_CLIENT_IP']))
+    {
+        $ip=$_SERVER['HTTP_CLIENT_IP'];
+    }
+    elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) //Proxy den bağlanıyorsa gerçek IP yi alır.
+
+    {
+        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    }
+   elseif (!empty($_SERVER['HTTP_X_FORWARDED'])) //Proxy den bağlanıyorsa gerçek IP yi alır.
+
+    {
+        $ip = $_SERVER['HTTP_X_FORWARDED'];
+    }
+
+   elseif (!empty($_SERVER['HTTP_X_CLUSTER_CLIENT_IP'])) //Proxy den bağlanıyorsa gerçek IP yi alır.
+
+    {
+        $ip = $_SERVER['HTTP_X_CLUSTER_CLIENT_IP'];
+    }
+	   elseif (!empty($_SERVER['HTTP_FORWARDED'])) //Proxy den bağlanıyorsa gerçek IP yi alır.
+
+    {
+        $ip = $_SERVER['HTTP_FORWARDED'];
+    }
+    else
+    {
+        $ip=$_SERVER['REMOTE_ADDR'];
+    }
+    return $ip;
+}
+
+ public function ErrorMessage($type, $method) {
+ 	die('<!--
+ 		Author : Ali Can Gönüllü
+ 		2020-2021
+ 		Respect !
+ 		-->
+<head><meta name="viewport" content="width=device-width, initial-scale=1.0">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
+    <link rel="stylesheet" media="screen" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600" />
+    <link rel="stylesheet" media="screen" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" />
+<link rel="stylesheet" media="screen" href="style.css" /></head>
+  <body class="background error-page-wrapper background-color background-image">
+    <center>
+  <div class="content-container shadow">
+  <br>
+    <div class="head-line secondary-text-color">
+		Illegal Girişim Algılandı
+    </div>
+	<div class="hr"></div>
+    <div class="context primary-text-color">
+      Deneme Türü : '.strip_tags($type).' ('.strip_tags($method).')
+    </div>
+    <div class="hr"></div>
+    <div class="context secondary-text-color">
+	<p> Method Türü : '.strip_tags($method).'</p>
+      <p>URL : <br>'.strip_tags($_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI']).'</p>
+	  <p>User-Agent : <br>'.strip_tags($_SERVER['HTTP_USER_AGENT']).'</p>
+	  <p>IP : <br>'.strip_tags($this->reel_ip()).'</p>
+	  <p>Tarih : '.date('d.m.Y H:i:s').'</p>
+    </div><br>
+    <div class="buttons-container">
+      <a class="button" onclick="history.back();" target="_blank"><span class="fa fa-home"></span> Geri Dön</a>
+      <a class="button" href="mailto:alicangonullu@yahoo.com" target="_blank"><span class="fa fa-warning"></span> Problem Bildir</a>
+    </div></body>');
+ }
+public function IPError($ad) {
+if(substr($_SERVER['HTTP_ACCEPT_LANGUAGE'],0,2) == "tr") {
+echo '<br><div class="context secondary-text-color">
+<p>IP Adresiniz '.strip_tags($ad).' Saat Banlandı.</p>
+</div>';
+} else {
+echo '<br><div class="context secondary-text-color">
+<p>IP Adress : '.strip_tags($ad).' Banned from Web Server for One Hour.</p>
+</div>';
+}
+}
+
+public function LogIslem($ad) {
+foreach ($ad as $key => $value) {
+$json = "".strip_tags($key)."¿".strip_tags($value)."";
+$yasakla = explode('¿',$json);
+$json2 = var_dump(json_encode($yasakla));
+	}
+}
+
+public function HeaderIslem() {
+$header = apache_request_headers();
+foreach ($header as $headers => $value) {
+$jsons = ''.strip_tags($headers).'¿?'.strip_tags($value).'';
+$yasakla = explode('¿?',$jsons);
+$jsonk = var_dump(json_encode($yasakla));
+	}
+}
+
+public function Debug() {
+echo '<pre class="container">';
+if($_SERVER['REQUEST_METHOD'] == "GET") {
+$getlog = LogIslem($_GET);
+echo '<br>';
+$gethead = HeaderIslem();
+} elseif($_SERVER['REQUEST_METHOD'] == "POST")
+{
+$getpost = LogIslem($_POST);
+echo '<br>';
+$gethead = HeaderIslem();
+} else {
+$getpost = LogIslem($_SERVER['REQUEST_METHOD']);
+echo '<br>';
+$gethead = HeaderIslem();
+}
+echo '</pre>';
+}
+
+public function memlimit($limit, $type) {
+	ini_set('memory_limit',''.$limit.''.$type.'');
+}
+
+public function kisalt($metin, $uzunluk){
+$metin = substr($metin, 0, $uzunluk)."...";
+$metin_son = strrchr($metin, " ");
+$metin = str_replace($metin_son," ...", $metin);
+return $metin;
+}
+
+public function prepareDB_OtobanDurum() {
+	$stmt = $this->aliwaf->query("SELECT * FROM waf_ayar ORDER BY ayar_id");
+	if($stmt->rowCount()) {
+		while($row = $stmt->fetch()){
+			if($row["oto_ban"] == "1") {
+				return true;
+			} else {
+				return false;
+			}
+		}
+	}
+}
+
+public function prepareDB_KontrolAyar() {
+	$stmt = $this->aliwaf->query("SELECT * FROM waf_ayar ORDER BY ayar_id");
+	if($stmt->rowCount()) {
+		while($row = $stmt->fetch()){
+			if($row["ayar_aktif"] == "1") {
+				return true;
+			} else {
+				return false;
+			}
+		}
+	}
+}
+
+public function prepareDB_WAFDurum() {
+	$stmt = $this->aliwaf->query("SELECT * FROM waf_ayar ORDER BY ayar_id");
+	if($stmt->rowCount()) {
+		while($row = $stmt->fetch()){
+			if($row["waf_aktif"] == "1") {
+				return true;
+			} else {
+				return false;
+			}
+		}
+	}
+}
+
+public function prepareDB_DebugDurum() {
+	$stmt = $this->aliwaf->query("SELECT * FROM waf_ayar ORDER BY ayar_id");
+	if($stmt->rowCount()) {
+		while($row = $stmt->fetch()){
+			if($row["debug"] == "1") {
+				return true;
+			} else {
+				return false;
+			}
+		}
+	}
+}
+
+public function insertDB_LogGiris($ad, $url, $header) {
+  $update = $this->aliwaf->prepare("INSERT INTO vuln_log(vuln_name, vuln_ip, vuln_url, vuln_header, vuln_date) VALUES (:ad, :ip, :url, :header, :dte) ");
+  $update->bindValue(':ad', strip_tags($ad));
+  $update->bindValue(':ip', strip_tags($this->reel_ip()));
+  $update->bindValue(':url', strip_tags($url));
+  $update->bindValue(':header', $header);
+  $update->bindValue(':dte', date("Y/m/d H:i:s"));
+  $update->execute();
+  if($rowz = $update->rowCount()) {
+  } else {
+  }
+}
+
+public function prepareDB_IPBan($ip) {
+	$stmt = $this->aliwaf->query("SELECT * FROM ip_ban WHERE ip_adresi = ".$this->aliwaf->quote($ip)."");
+	if($row = $stmt->rowCount()) {
+		$this->ErrorMessage("IP Ban", strip_tags($ip));
+	} else {
+		//Normal
+	}
+}
+
+public function queryDB_KontrolKurali($data) {
+	$stmt = $this->aliwaf->query('SELECT * FROM guard_watch ORDER BY kural_id');
+	while($row = $stmt->fetch()){
+		$parametreler = strtolower(urldecode($data));
+		$parametreler0 = str_replace("#", "", $parametreler);
+		$parametreler1 = str_replace("!", "", $parametreler0);
+		$parametreler2 = str_replace("=", "", $parametreler1);
+		$parametreler3 = str_replace("&", "", $parametreler2);
+		$parametreler8 = str_replace("-", "", $parametreler3);
+		$yasaklar=$row['kural_icerik'];
+		$yasakla=explode('¿¿',$yasaklar);
+		$sayiver=substr_count($yasaklar,'¿¿');
+		$i=0;
+		while ($i<=$sayiver) {
+			if (strstr($parametreler8,$yasakla[$i])) {
+				$this->insertDB_LogGiris("Injection Error", $parametreler, "");
+				$this->ErrorMessage("Injection Error ", strip_tags("Type : ".$this->kisalt($parametreler, 50)." | ".$row['kural_adi'].""));
+				if($this->prepareDB_OtobanDurum()  == true){
+					$this->prepareDB_IPBan($this->reel_ip());
+				} else {
+					die();
+				}
+			}
+			$i++;
+		}
+	}
+}
+
+public function queryDB_MethodKontrol($method) {
+	$stmt = $this->aliwaf->query("SELECT * FROM method_blok WHERE method_turu = ".$this->aliwaf->quote($method)."");
+	if($row = $stmt->rowCount()) {
+		
+	} else {
+		header($_SERVER["SERVER_PROTOCOL"]." 405 Method Not Allowed", true, 405);
+		$this->insertDB_LogGiris("Method Error", $_SERVER["SERVER_PROTOCOL"], "");
+		$this->ErrorMessage('Method Error', strip_tags($this->reel_ip()));
+		if($this->prepareDB_OtobanDurum()  == true){
+			$this->prepareDB_IPBan($this->reel_ip());
+		} else {
+			die();
+		}
+	}
+}
+
+}
--- engelle.php
+++ engelle.php
@@ -1,330 +1,36 @@
 <?php
-error_reporting(0);
-include("conn.php");
 ob_start();
-function reel_ip() {
-    if (!empty($_SERVER['HTTP_CLIENT_IP']))
-    {
-        $ip=$_SERVER['HTTP_CLIENT_IP'];
-    }
-    elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) //Proxy den bağlanıyorsa gerçek IP yi alır.
+include("class.engelle.php");
 
-    {
-        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-    }
-   elseif (!empty($_SERVER['HTTP_X_FORWARDED'])) //Proxy den bağlanıyorsa gerçek IP yi alır.
+$waf = new AliWAF_Block();
+$waf->Baglanti();
+$waf->memlimit("256", "MB");
 
-    {
-        $ip = $_SERVER['HTTP_X_FORWARDED'];
-    }
-
-   elseif (!empty($_SERVER['HTTP_X_CLUSTER_CLIENT_IP'])) //Proxy den bağlanıyorsa gerçek IP yi alır.
-
-    {
-        $ip = $_SERVER['HTTP_X_CLUSTER_CLIENT_IP'];
-    }
-	   elseif (!empty($_SERVER['HTTP_FORWARDED'])) //Proxy den bağlanıyorsa gerçek IP yi alır.
-
-    {
-        $ip = $_SERVER['HTTP_FORWARDED'];
-    }
-    else
-    {
-        $ip=$_SERVER['REMOTE_ADDR'];
-    }
-    return $ip;
+if ($waf->prepareDB_DebugDurum() == true){
+$waf->Debug();
 }
 
-function ErrorMessage($type, $method) {
-die('<!--
-Author : Ali Can Gönüllü
-2020-2021
-Respect !
--->
-<head><meta name="viewport" content="width=device-width, initial-scale=1.0">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
-    <link rel="stylesheet" media="screen" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600" />
-    <link rel="stylesheet" media="screen" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" />
-<link rel="stylesheet" media="screen" href="style.css" /></head>
-  <body class="background error-page-wrapper background-color background-image">
-    <center>
-  <div class="content-container shadow">
-  <br>
-    <div class="head-line secondary-text-color">
-		Illegal Girişim Algılandı
-    </div>
-	<div class="hr"></div>
-    <div class="context primary-text-color">
-      Deneme Türü : '.strip_tags($type).' ('.strip_tags($method).')
-    </div>
-    <div class="hr"></div>
-    <div class="context secondary-text-color">
-	<p> Method Türü : '.strip_tags($method).'</p>
-      <p>URL : <br>'.strip_tags($_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI']).'</p>
-	  <p>User-Agent : <br>'.strip_tags($_SERVER['HTTP_USER_AGENT']).'</p>
-	  <p>IP : <br>'.strip_tags(reel_ip()).'</p>
-	  <p>Tarih : '.date('d.m.Y H:i:s').'</p>
-    </div><br>
-    <div class="buttons-container">
-      <a class="button" onclick="history.back();" target="_blank"><span class="fa fa-home"></span> Geri Dön</a>
-      <a class="button" href="mailto:alicangonullu@yahoo.com" target="_blank"><span class="fa fa-warning"></span> Problem Bildir</a>
-    </div></body>');
-}
-function IPError($ad) {
-if(substr($_SERVER['HTTP_ACCEPT_LANGUAGE'],0,2) == "tr") {
-echo '<br><div class="context secondary-text-color">
-<p>IP Adresiniz '.strip_tags($ad).' Saat Banlandı.</p>
-</div>';
-} else {
-echo '<br><div class="context secondary-text-color">
-<p>IP Adress : '.strip_tags($ad).' Banned from Web Server for One Hour.</p>
-</div>';
-}
-}
+if($waf->prepareDB_WAFDurum() == true) {
+header("X-AliWAF: ACTIVE");
+if ($waf->prepareDB_KontrolAyar() == true){
 
-function LogIslem($ad) {
-foreach ($ad as $key => $value) {
-$json = "".strip_tags($key)."¿".strip_tags($value)."";
-$yasakla = explode('¿',$json);
-$json2 = var_dump(json_encode($yasakla));
-	}
-}
 
-function HeaderIslem() {
-$header = apache_request_headers();
-foreach ($header as $headers => $value) {
-$jsons = ''.strip_tags($headers).'¿?'.strip_tags($value).'';
-$yasakla = explode('¿?',$jsons);
-$jsonk = var_dump(json_encode($yasakla));
-	}
-}
-
-function Debug() {
-echo '<pre class="container">';
-if($_SERVER['REQUEST_METHOD'] == "GET") {
-$getlog = LogIslem($_GET);
-echo '<br>';
-$gethead = HeaderIslem();
-} elseif($_SERVER['REQUEST_METHOD'] == "POST")
-{
-$getpost = LogIslem($_POST);
-echo '<br>';
-$gethead = HeaderIslem();
-} else {
-$getpost = LogIslem($_SERVER['REQUEST_METHOD']);
-echo '<br>';
-$gethead = HeaderIslem();
-}
-echo '</pre>';
-}
-
-function memlimit($limit, $type) {
-	ini_set('memory_limit',''.$limit.''.$type.'');
-}
-
-function kisalt($metin, $uzunluk){
-$metin = substr($metin, 0, $uzunluk)."...";
-$metin_son = strrchr($metin, " ");
-$metin = str_replace($metin_son," ...", $metin);
-return $metin;
-}
-
-try {
-
-$stmt = $aliwaf->query("SELECT * FROM waf_ayar ORDER BY ayar_id");
-if($stmt->rowCount()) {
-while($row = $stmt->fetch()){
-session_start();
-$ayaraktif = md5(sha1($row["ayar_aktif"]));
-$otoban = md5(sha1($row["oto_ban"]));
-$ipadres = reel_ip();
-$wafdurum = md5(sha1($row["waf_aktif"]));
-$debug = md5(sha1($row["debug"]));
-if (md5(sha1($row["ayar_aktif"])) == md5(sha1(1))){
-header('X-AliWAF: ACTIVE');
-} else {
-header('X-AliWAF: DEACTIVE');
-}
-   }
-		}
-		    } catch(PDOException $e) {
-}
-
-if ($wafdurum == md5(sha1(1))){
-if ($debug == md5(sha1(1))){
-Debug();
-} else {
-}
-memlimit("256", "MB");
-if ($ayaraktif == md5(sha1(1))){
-$ip = reel_ip();
-$stmt = $aliwaf->query("SELECT * FROM ip_ban WHERE ip_adresi = ".$aliwaf->quote($ip)."");
-if($stmt->rowCount()) {
-while($row = $stmt->fetch()){
-session_start();
-$suresi = strip_tags($row["ip_suresi"]);
-$ipusragent = strip_tags($row["ip_usragent"]);
-
-if($ipusragent == "panel") {
-  $update = $aliwaf->prepare("INSERT INTO vuln_log(vuln_name, vuln_ip, vuln_url, vuln_header, vuln_date) VALUES (:ad, :ip, :url, :header, :dte) ");
-  $update->bindValue(':ad', strip_tags("IP Ban"));
-  $update->bindValue(':ip', strip_tags(reel_ip()));
-  $update->bindValue(':url', strip_tags("IP"));
-  $update->bindValue(':header', strip_tags($_SERVER['HTTP_X_REQUESTED_WITH']));
-  $update->bindValue(':dte', date("Y/m/d H:i:s"));
-  $update->execute();
-  while($rowz = $update->fetch()){
-  }
-ErrorMessage("IP Ban | Unlimited", strip_tags($ip));
-} else {
-if ($suresi - date('H:i:s') >= 30){
-} else {
-ErrorMessage("IP Ban", strip_tags($ip));
-die();
-		}
-}
-
-}
-}
+$waf->queryDB_KontrolKurali(file_get_contents('php://stdin'));
 
 if($_POST) {
-$stmt = $aliwaf->query('SELECT * FROM guard_watch ORDER BY kural_id');
-while($row = $stmt->fetch()){
-$parametreler = strtolower(urldecode(file_get_contents('php://input')));
-$parametreler0 = str_replace("#", "", $parametreler);
-$parametreler1 = str_replace("!", "", $parametreler0);
-$parametreler2 = str_replace("=", "", $parametreler1);
-$parametreler3 = str_replace("&", "", $parametreler2);
-$parametreler8 = str_replace("-", "", $parametreler3);
-$yasaklar=$row['kural_icerik'];
-$yasakla=explode('¿¿',$yasaklar);
-$sayiver=substr_count($yasaklar,'¿¿');
-$i=0;
-while ($i<=$sayiver) {
-if (strstr($parametreler8,$yasakla[$i])) {
-  $update = $aliwaf->prepare("INSERT INTO vuln_log(vuln_name, vuln_ip, vuln_url, vuln_header, vuln_date) VALUES (:ad, :ip, :url, :header, :dte) ");
-  $update->bindValue(':ad', strip_tags("POST Injection | ".$row['kural_adi'].""));
-  $update->bindValue(':ip', strip_tags(reel_ip()));
-  $update->bindValue(':url', strip_tags($parametreler));
-  $update->bindValue(':header', strip_tags($_SERVER['HTTP_X_REQUESTED_WITH']));
-  $update->bindValue(':dte', date("Y/m/d H:i:s"));
-  $update->execute();
-  while($rowz = $update->fetch()){
-  }
-ErrorMessage("POST Injection", strip_tags("Type : ".kisalt($parametreler, 50)." | ".$row['kural_adi'].""));
-
-if ($otoban == md5(sha1(1))){
-$bandurum = md5(sha1(1));
-$update = $aliwaf->prepare("INSERT INTO ip_ban(ip_adresi, ip_suresi, ip_usragent) VALUES (:ipadresi, :ipsuresi, :ipusragent) ");
-$update->bindValue(':ipadresi', strip_tags(reel_ip()));
-$update->bindValue(':ipusragent', strip_tags($_SERVER['HTTP_USER_AGENT']));
-$update->bindValue(':ipsuresi', date('H:i:s'));
-$update->execute();
-if($update){
-IPError("1");
-die();
-}
+$waf->queryDB_KontrolKurali(file_get_contents('php://input'));
 } else {
-die();
+$waf->queryDB_KontrolKurali($_SERVER['QUERY_STRING']);
 }
 
+$waf->queryDB_MethodKontrol(strip_tags($_SERVER['REQUEST_METHOD']));
+} else {
+die("AliWAF Error : Configs are not active");
+header("X-AliWAF: DEACTIVE");
 }
-$i++;
-}
-	}
-
 
 } else {
-
-$stmt = $aliwaf->query('SELECT * FROM guard_watch ORDER BY kural_id');
-while($row = $stmt->fetch()){
-$parametreler = strtolower($_SERVER['QUERY_STRING']);
-$parametreler0 = str_replace("#", "", $parametreler);
-$parametreler1 = str_replace("!", "", $parametreler0);
-$parametreler2 = str_replace("=", "", $parametreler1);
-$parametreler3 = str_replace("&", "", $parametreler2);
-$parametreler8 = str_replace("-", "", $parametreler3);
-$yasaklar=($row['kural_icerik']);
-$yasakla=explode('¿¿',$yasaklar);
-$sayiver=substr_count($yasaklar,'¿¿');
-$i=0;
-while ($i<=$sayiver) {
-if (strstr($parametreler8,$yasakla[$i])) {
-  $update = $aliwaf->prepare("INSERT INTO vuln_log(vuln_name, vuln_ip, vuln_url, vuln_header, vuln_date) VALUES (:ad, :ip, :url, :header, :dte) ");
-  $update->bindValue(':ad', strip_tags("POST Injection | ".$row['kural_adi'].""));
-  $update->bindValue(':ip', strip_tags(reel_ip()));
-  $update->bindValue(':url', strip_tags($parametreler));
-  $update->bindValue(':header', strip_tags($_SERVER['HTTP_X_REQUESTED_WITH']));
-  $update->bindValue(':dte', date("Y/m/d H:i:s"));
-  $update->execute();
-  while($rowz = $update->fetch()){
-  }
-ErrorMessage("Rule Injection", strip_tags($row['kural_adi']));
-
-if ($otoban == md5(sha1(1))){
-$bandurum = md5(sha1(1));
-$update = $aliwaf->prepare("INSERT INTO ip_ban(ip_adresi, ip_suresi, ip_usragent) VALUES (:ipadresi, :ipsuresi, :ipusragent) ");
-$update->bindValue(':ipadresi', strip_tags($ip));
-$update->bindValue(':ipusragent', strip_tags($_SERVER['HTTP_USER_AGENT']));
-$update->bindValue(':ipsuresi', date('H:i:s'));
-$update->execute();
-if($update){
-IPError("1");
-die();
+header("X-AliWAF: DEACTIVE");
 }
-} else {
-die();
-}
-}
-$i++;
-}
-if (strlen($parametreler8)>=90) {
-exit;
-}
-}
-}
-    //Guard Bitti
-
-$method = strip_tags($_SERVER['REQUEST_METHOD']);
-$stmt = $aliwaf->query("SELECT * FROM method_blok WHERE method_turu = ".$aliwaf->quote($method)."");
-if($stmt->rowCount()) {
-while($row = $stmt->fetch()){
-}
-} else {
-header($_SERVER["SERVER_PROTOCOL"]." 405 Method Not Allowed", true, 405);
-  $update = $aliwaf->prepare("INSERT INTO vuln_log(vuln_name, vuln_ip, vuln_url, vuln_header, vuln_date) VALUES (:ad, :ip, :url, :header, :dte) ");
-  $update->bindValue(':ad', strip_tags("Not Permission Method Try | ".$row['kural_adi'].""));
-  $update->bindValue(':ip', strip_tags(reel_ip()));
-  $update->bindValue(':url', strip_tags($method));
-  $update->bindValue(':header', strip_tags($_SERVER['HTTP_X_REQUESTED_WITH']));
-  $update->bindValue(':dte', date("Y/m/d H:i:s"));
-  $update->execute();
-  while($rowz = $update->fetch()){
-  }
-ErrorMessage("Method Injection", strip_tags($method));
-if ($otoban == md5(sha1(1))){
-$bandurum = md5(sha1(1));
-$update = $aliwaf->prepare("INSERT INTO ip_ban(ip_adresi, ip_suresi, ip_usragent) VALUES (:ipadresi, :ipsuresi, :ipusragent) ");
-$update->bindValue(':ipadresi', strip_tags($ip));
-$update->bindValue(':ipusragent', strip_tags($_SERVER['HTTP_USER_AGENT']));
-$update->bindValue(':ipsuresi', date('H:i:s'));
-$update->execute();
-if($update){
-IPError("1");
-die();
-}
-} else {
-
-}
-
-}
-} else {
-header('X-AliWAF: CONFIG-DEACTIVE');
-}
-} else {
-header('X-AliWAF: DEACTIVE');
-}
-	//Istek Engellendi
-
+$waf->closeConnection();
 ?>
