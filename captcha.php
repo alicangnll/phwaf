062df6fcfb86a2cf6daeab35ffad948e3abb4d13
Ali Can Gönüllü <alicangonullu@yahoo.com>
2022-09-18 01:07:58 +0300

    PHP Ali WAF (17.09.2022 / v1.2.4) Update
    PHP Ali WAF (17.09.2022 / v1.2.4) Update

--- captcha.php
+++ captcha.php
@@ -1,3 +1,21 @@
 <?php
-$aliwafac->LoginCheck_Captcha();
+include("class.aliwaf.php");
+$aliwafac = new AliWAF_Panel();
+if(extension_loaded("gd")) {
+	header('Content-type: image/png');
+	  session_start();
+      $kod = substr(md5(uniqid(rand(0, 6))), 0, 6);
+      $_SESSION['kod'] = $kod;
+      $kod_uzunluk = strlen($kod);
+      $genislik = imagefontwidth(5) * $kod_uzunluk;
+      $yukseklik = imagefontheight(5);
+      $resim = imagecreate($genislik, $yukseklik);
+      $arka_renk = imagecolorallocate($resim, 0, 0, 0);
+      $yazi_renk = imagecolorallocate($resim, 255, 255, 255);
+      imagefill($resim, 0, 0, $arka_renk);
+      imagestring($resim, 5, 0, 0, $kod, $yazi_renk);
+      imagepng($resim);
+} else {
+	die("404");
+}
 ?>
--- class.aliwaf.php
+++ class.aliwaf.php
@@ -20,21 +20,7 @@ class AliWAF_Panel {
 	}
 
   private function Captcha_Olustur() {
-    if(extension_loaded("gd")) {
-      session_start();
-      $kod = substr(md5(uniqid(rand(0, 6))), 0, 6);
-      $_SESSION['kod'] = $kod;
-      header('Content-type: image/png');
-      $kod_uzunluk = strlen($kod);
-      $genislik = imagefontwidth(5) * $kod_uzunluk;
-      $yukseklik = imagefontheight(5);
-      $resim = imagecreate($genislik, $yukseklik);
-      $arka_renk = imagecolorallocate($resim, 0, 0, 0);
-      $yazi_renk = imagecolorallocate($resim, 255, 255, 255);
-      imagefill($resim, 0, 0, $arka_renk);
-      imagestring($resim, 5, 0, 0, $kod, $yazi_renk);
-      imagepng($resim);
-    } else {
+    if(!extension_loaded("gd")) {
       $kod = substr(md5(uniqid(rand(0, 6))), 0, 6);
       $_SESSION['kod'] = $kod;
       return base64_encode($kod);
@@ -46,7 +32,7 @@ class AliWAF_Panel {
     echo '
     <div class="col-md-12 form-group">';
     if(extension_loaded("gd")) {
-    echo '<img src="data:image/png;base64,'.base64_encode($this->Captcha_Olustur()).'" /><br>';
+    echo '<center><img src="captcha.php"/><br></center>';
     } else {
       echo '<b>CAPTCHA Kodunuz : '.base64_decode($this->Captcha_Olustur()).'</b>';
     }
